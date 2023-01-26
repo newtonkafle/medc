4951c1d8071dca5a885b51ba1cfe6ddaf8985ad3
Newton <nkafle49@gmail.com>
2023-01-26 22:33:40 +1030

    updated database

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap5
+from model import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
 bootstrap = Bootstrap5(app)
-db = SQLAlchemy(app)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def home():
-    db.create_all()
     return render_template('product.html')
 
 
--- model.py
+++ model.py
@@ -1,12 +1,14 @@
-from main import db
+import sqlalchemy as sa
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
 
-class Products(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(100), unique=True)
-    selling_price = db.Column(db.Integer, nullabe=False)
+    selling_price = db.Column(db.Integer, nullable=False)
     discount = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(1000), nullabe=False)
-    brand = db.Column(db.String(100), nullabe=False)
+    description = db.Column(db.String(1000), nullable=False)
+    brand = db.Column(db.String(100), nullable=False)
     image = db.Column(db.String(200))
     category = db.Column(db.String(20), nullable=False)
