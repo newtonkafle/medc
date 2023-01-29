72658874edf17953eda83099fa3f778e10e9e4f0
Newton <nkafle49@gmail.com>
2023-01-29 20:47:53 +1030

    register page created

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms import StringField, SubmitField, PasswordField, EmailField, Label
 from wtforms.validators import DataRequired, Length, Email
 
 
@@ -11,8 +11,26 @@ class SearchBar(FlaskForm):
 
 class Login(FlaskForm):
     email = EmailField('Email address', validators=[DataRequired(), Length(
-        max=100), Email(message='Type correct email address')], render_kw={'placeholder': 'Enter email here'})
+        max=100), Email(message='Type correct email address')], render_kw={'placeholder': 'Enter email here..'})
     password = PasswordField("Password", validators=[DataRequired()], render_kw={
-                             'placeholder': 'Enter password here'})
+                             'placeholder': 'Enter password here..'})
+    forgot_password = Label(text='Forgot password?',
+                            field_id='forgot_password')
     login = SubmitField("Login")
+    google_login = SubmitField("Sign In using Google Account")
+
+
+class Register(FlaskForm):
+    first_name = StringField('First Name', validators=[
+                             DataRequired(), Length(max=50)], render_kw={'placeholder': 'Firstname..'})
+    last_name = StringField('Last Name', validators=[
+                            DataRequired(), Length(max=50)], render_kw={'placeholder': 'Lastname..'})
+    middle_name = StringField('Middle Name', validators=[
+                              DataRequired(), Length(max=50)], render_kw={'placeholder': 'Middlename..'})
+    email = EmailField('Email address', validators=[DataRequired(), Length(
+        max=100), Email(message='Type correct email address')], render_kw={'placeholder': 'Enter email here..'})
+    password = PasswordField("Password", validators=[DataRequired()], render_kw={
+                             'placeholder': 'Enter password here..'})
+
+    submit = SubmitField("Submit")
     google_login = SubmitField("Sign In using Google Account")
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import datetime
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap5
 from model import db
 from model import Product
-from forms import SearchBar, Login
+from forms import SearchBar, Login, Register
 
 
 app = Flask(__name__)
@@ -26,8 +27,19 @@ def product():
 # User login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # calculating year for footer
     login_form = Login()
-    return render_template("login.html", form=login_form)
+    return render_template("login.html", form=login_form, year=calc_year())
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    register_form = Register()
+    return render_template("sign_up.html", form=register_form, year=calc_year())
+
+
+def calc_year():
+    return datetime.date.today().year
 
 
 if __name__ == '__main__':
